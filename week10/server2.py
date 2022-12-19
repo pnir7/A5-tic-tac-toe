74c47d3e59ac14c340f4a2d7088a0e6f6f9cd482
Anqi Pan <pananqi@MacBook-Pro-309.local>
2022-12-19 10:47:32 -0800

    updated flask server

--- week10/server2.py
+++ week10/server2.py
@@ -1,6 +1,10 @@
 import random
 from flask import Flask, request, make_response, render_template, redirect
 
+from database import database
+
+db = database()
+
 _server = Flask(__name__)
 
 def make_empty_board():
@@ -46,34 +50,65 @@ board = make_empty_board()
 def index():
     if request.method == 'POST':
         name = request.form.get('name')
-        resp = make_response()
+        if not name:
+            return render_template('index.html', error='Please enter your name')
+        mode = request.form.get('mode')
+        resp = make_response(redirect('/play'))
         resp.set_cookie('name', name)
+        resp.set_cookie('mode', mode)
         board[:] = make_empty_board()
-        return redirect('/play')
-    return render_template('index.html')
+        return resp
+    
+    name = request.cookies.get('name', '')
+    return render_template('index.html', name=name)
 
 @_server.route('/play', methods=['GET', 'POST'])
 def play():
+    name = request.cookies.get('name')
+    mode = request.cookies.get('mode')
+    if not name or not mode:
+        return redirect('/')
+
+    player = request.form.get('player', 'X')
     if request.method == 'POST':
         x = int(request.form.get('x'))
         y = int(request.form.get('y'))
         if not board[x][y]:
-            board[x][y] = 'X'
+            board[x][y] = player
             winner = get_winner(board)
             if not winner:
-                result = gen_num()
-                if result:
-                    row, col = result
-                    board[row][col] = 'O'
-                    winner = get_winner(board)
+                if mode == '1':
+                    result = gen_num()
+                    if result:
+                        row, col = result
+                        board[row][col] = 'O'
+                        winner = get_winner(board)
+                    else:
+                        winner = 'draw'
                 else:
-                    winner = 'draw'
+                    player = 'O' if player == 'X' else 'X'
         else:
             winner = get_winner(board)
+        
+        if mode == '1' and winner:
+            if winner == 'draw':
+                db.record_game(name, 'draw')
+            elif winner == 'X':
+                db.record_game(name, 'win')
+            else:
+                db.record_game(name, 'lose')
     else:
         winner = get_winner(board)
+    
+    return render_template('play.html', mode=mode, board=board, player=player, winner=winner)
 
-    return render_template('play.html', board=board, winner=winner)
+
+
+@_server.route('/stats')
+def stats():
+    ranking_list = db.get_ranking_list()
+    print(ranking_list)
+    return render_template('stats.html', ranking_list=ranking_list)
 
 
 if __name__ == '__main__':
